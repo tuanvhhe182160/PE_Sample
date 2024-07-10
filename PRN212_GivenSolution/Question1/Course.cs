234944eafca8d39bdfa8ec23f4be920d7bccd5e5
tuanvhhe182160 <tuanvhhe182160@fpt.edu.vn>
2024-07-10 13:43:11 +0700

    Add files via upload

---
+++ PRN212_GivenSolution/Question1/Course.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    public class Course
+    {
+        public int CourseID { get; private set; }
+        public string CourseTitle { get; private set; }
+        private Dictionary<Student, double> Students;
+        public delegate void NumberOfStudentChangeHandler(int oldNumber, int newNumber);
+        public event NumberOfStudentChangeHandler OnNumberOfStudentChange;
+
+        public Course(int courseID, string courseTitle)
+        {
+            CourseID = courseID;
+            CourseTitle = courseTitle;
+            Students = new Dictionary<Student, double>();
+        }
+        public Course() { }
+        public void AddStudent(Student p, double g)
+        {
+            int oldCount = Students.Count;
+            Students[p] = g;
+            int newCount = Students.Count;  
+            OnNumberOfStudentChange?.Invoke(oldCount, newCount);
+        }
+        public void RemoveStudent(int StudentID)
+        {
+            int oldCount = Students.Count;
+            foreach (var p in Students.Keys)
+            {
+                if(p.StudentID == StudentID)
+                {
+                    Students.Remove(p);
+                }
+            }
+            int newCount = Students.Count;
+            OnNumberOfStudentChange?.Invoke(oldCount, newCount);
+        }
+        public override string ToString()
+        {
+            string result = $"Course: {CourseID} {CourseTitle}\n";
+            foreach (var p in Students)
+            {
+                result += $"Course: - {p.Key} - Mark: {p.Value}\n";
+            }
+            return result;
+        }
+    }
+}
---
+++ PRN212_GivenSolution/Question1/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1
+{
+    public class Student
+    {
+        public int StudentID { get; private set; }
+        public string StudentName { get; private set; }
+
+        public Student(int StudentID, string StudentName)
+        {
+            this.StudentID = StudentID;
+            this.StudentName = StudentName;
+        }
+        public Student() { }
+        public override string ToString()
+        {
+            return $"{StudentID} : {StudentName}";
+        }
+    }
+}
